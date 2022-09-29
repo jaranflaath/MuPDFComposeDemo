ec1bab249f4cdfb887350749dc203584cecb9b50
Jaran Flaath <jaran.flaath@hey.com>
2022-09-29 17:24:15 +0200

    DocumentView.initialize at beginning of onCreate

--- app/src/main/java/no/agens/mupdfcomposedemo/MainActivity.kt
+++ app/src/main/java/no/agens/mupdfcomposedemo/MainActivity.kt
@@ -26,6 +26,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        DocumentView.initialize(this)
+
         setContent {
 
                 MuPDFComposeDemoTheme {
